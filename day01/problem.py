5e4547cab0e2fed7124242006a90bd09b4b8e83b
Joaquín Bernal <jobedom@gmail.com>
2020-12-03 21:11:52 +0100

    Reformat code

--- day01/problem.py
+++ day01/problem.py
@@ -8,11 +8,11 @@ numbers = get_file_numbers('./input.txt')
 number_count = len(numbers)
 
 for i, j in unique_index_pairs(number_count):
-   if numbers[i] + numbers[j] == 2020:
-      print(f'Part 1: {numbers[i] * numbers[j]}')
-      break
+    if numbers[i] + numbers[j] == 2020:
+        print(f'Part 1: {numbers[i] * numbers[j]}')
+        break
 
 for i, j, k in unique_index_triplets(number_count):
-   if numbers[i] + numbers[j] + numbers[k] == 2020:
-      print(f'Part 2: {numbers[i] * numbers[j] * numbers[k]}')
-      break
+    if numbers[i] + numbers[j] + numbers[k] == 2020:
+        print(f'Part 2: {numbers[i] * numbers[j] * numbers[k]}')
+        break
--- day03/problem.py
+++ day03/problem.py
@@ -1,5 +1,3 @@
-import sys
-import re
 import math
 
 from misc import init
@@ -9,15 +7,17 @@ init(__file__)
 
 forest = get_file_lines('./input.txt')
 
+
 def get_tree_count(slope_horizontal, slope_vertical):
-   tree_count = 0
-   pos = 0
-   for index, row in enumerate(forest):
-      if index % slope_vertical == 0:
-         if row[pos] == '#':
-            tree_count += 1
-         pos = (pos + slope_horizontal) % len(row)
-   return tree_count
+    tree_count = 0
+    pos = 0
+    for index, row in enumerate(forest):
+        if index % slope_vertical == 0:
+            if row[pos] == '#':
+                tree_count += 1
+            pos = (pos + slope_horizontal) % len(row)
+    return tree_count
+
 
 result_1 = get_tree_count(3, 1)
 result_2 = math.prod(get_tree_count(h, v) for (h, v) in [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)])
